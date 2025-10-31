12b6260064ef8efb98d54b040af0ea0f2a8f114e
Mayank Grover <mayankgrover561@gmail.com>
2025-10-31 22:02:21 +0530

    External weather API Key integration (get call)

--- src/main/java/com/mayank/journalApp/controller/UserController.java
+++ src/main/java/com/mayank/journalApp/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.mayank.journalApp.controller;
 
 
+import com.mayank.journalApp.api.response.WeatherResponse;
 import com.mayank.journalApp.entity.User;
 import com.mayank.journalApp.service.UserService;
+import com.mayank.journalApp.service.WeatherServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private WeatherServices weatherServices;
 
     @PutMapping
     public ResponseEntity<?> updateUser(@RequestBody User user) {
@@ -36,5 +40,16 @@ public class UserController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         userService.deleteByUserName(authentication.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<String> greetings() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        WeatherResponse response = weatherServices.getWeather("Mumbai");
+        String greetings = "";
+        if(response != null){
+            greetings = String.format("Hi %s weather feels like %d",authentication.getName(),response.getCurrent().getFeelslike());
+        }
+        return new ResponseEntity<>(greetings,HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/mayank/journalApp/service/WeatherServices.java
@@ -0,0 +1,24 @@
+package com.mayank.journalApp.service;
+
+import com.mayank.journalApp.api.response.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class WeatherServices {
+
+    private static final String API_KEY = "#YOUR_API_KEY";
+    private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public WeatherResponse getWeather(String city){
+        String finalAPI = API.replace("CITY",city).replace("API_KEY",API_KEY);
+        ResponseEntity<WeatherResponse> exchange = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+        return exchange.getBody();
+    }
+}
