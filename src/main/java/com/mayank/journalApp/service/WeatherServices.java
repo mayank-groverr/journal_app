28f7693c56a3ea3eced08502a123f8a304689ef0
Mayank Grover <mayankgrover561@gmail.com>
2025-11-02 19:24:08 +0530

    added value annotation (not hardcoding the values)

--- src/main/java/com/mayank/journalApp/service/WeatherServices.java
+++ src/main/java/com/mayank/journalApp/service/WeatherServices.java
@@ -2,6 +2,7 @@ package com.mayank.journalApp.service;
 
 import com.mayank.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,15 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherServices {
 
-    private static final String API_KEY = "#YOUR_API_KEY";
+    @Value("${weather.api.key.value}")
+    private String apiKey;
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
     private RestTemplate restTemplate;
 
     public WeatherResponse getWeather(String city){
-        String finalAPI = API.replace("CITY",city).replace("API_KEY",API_KEY);
+        String finalAPI = API.replace("CITY",city).replace("API_KEY",apiKey);
         ResponseEntity<WeatherResponse> exchange = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
         return exchange.getBody();
     }
