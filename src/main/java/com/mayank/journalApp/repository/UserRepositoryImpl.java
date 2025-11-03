eca620f5fb4e169838169922bb5bfc3e2ed4cf16
Mayank Grover <mayankgrover561@gmail.com>
2025-11-03 21:34:10 +0530

    added criteria api functionality

---
+++ src/main/java/com/mayank/journalApp/repository/UserRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.mayank.journalApp.repository;
+
+import com.mayank.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUserForSentimentalAnalysis(){
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.com$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").ne(null).ne(""));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
+    }
+}
